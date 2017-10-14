87c0a8227dd866494531562e2d0ef36782b83f3e
Pascal Berger <bep@bbtsoftware.ch>
2017-10-14 11:49:31 +0200

    Remove not required using statements

--- src/Cake.Svn.Tests/Unit/Export/SvnExportSettingsTests.cs
+++ src/Cake.Svn.Tests/Unit/Export/SvnExportSettingsTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Cake.Svn.Export;
+﻿using Cake.Svn.Export;
 using Xunit;
 
 namespace Cake.Svn.Tests.Unit.Export
--- src/Cake.Svn/Internal/Extensions/SvnDepthExtensions.cs
+++ src/Cake.Svn/Internal/Extensions/SvnDepthExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cake.Svn.Internal.Extensions
+﻿namespace Cake.Svn.Internal.Extensions
 {
     internal static class SvnDepthExtensions
     {
